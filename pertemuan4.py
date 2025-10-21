bd3acb9789bb8f6b679d92e1907621f45bfa9c8c
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-10-21 14:05:45 +0700

    outputnya

--- pertemuan4.py
+++ pertemuan4.py
@@ -13,4 +13,14 @@ class PersegiPanjang:
         return f"Persegi Panjang, Panjang {self.panjang} cm, dan lebar {self.lebar} cm"
 
 
+try:
+    panjang_input = int(input("Masukan Panjang (cm):"))
+    lebar_input = int(input("Masukan Lebar (cm):")) 
+
+    pp = PersegiPanjang(panjang_input, lebar_input)
+    print("Keliling:", pp.keliling(), "cm")
+    print("Luas:", pp.luas(), "cm^2")
+
+except ValueError:
+    print("Input harus berupaangka.")
     
